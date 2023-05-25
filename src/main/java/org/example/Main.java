ca4a6466cbb61d282820abf4041296ced90a57a9
CSandu <CristinaAlexandra.Sandu@harman.com>
2023-05-25 14:06:57 +0300

    Added the jar's path to the dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,5 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-           File file = new File("OnlineServices-1.0-SNAPSHOT.jar");
-           String absolutePath = file.getAbsolutePath();
-           System.out.println(absolutePath);
     }
 }
