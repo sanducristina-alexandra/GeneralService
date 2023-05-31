813eb61763ec32cf91bfc0a1b77ae4af5dcbc6be
CSandu <CristinaAlexandra.Sandu@harman.com>
2023-05-31 11:38:50 +0300

    Created OnlineServiceHandler

---
+++ src/main/java/GeneralService/OnlineServiceHandler.java
@@ -0,0 +1,11 @@
+package GeneralService;
+
+import java.util.List;
+
+public class OnlineServiceHandler {
+    private List<String> services;
+
+    public OnlineServiceHandler(List<String> services) {
+        this.services = services;
+    }
+}
