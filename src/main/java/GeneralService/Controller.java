4d43fec1ed0da26bd214ba943002233cd439e880
Radu <radu1351@yahoo.com>
2023-06-12 12:47:55 +0300

    Modified OnlineServiceHandler

--- src/main/java/GeneralService/Controller.java
+++ src/main/java/GeneralService/Controller.java
@@ -1,18 +1,23 @@
 package GeneralService;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Future;
 
 @RestController
 public class Controller {
 
     public List<String> validCarUserIds = new ArrayList<>(Arrays.asList("50075", "15080"));
 
+    @Async
     @PostMapping("/request")
-    public Boolean processRequest(@RequestBody Request request) {
-        return validCarUserIds.contains(request.getUserId());
+    public Future<Boolean> processRequest(@RequestBody Request request) {
+        Boolean result = validCarUserIds.contains(request.getUserId());
+        return new AsyncResult<>(result);
     }
 
 }
--- src/main/java/GeneralService/OnlineServiceHandler.java
+++ src/main/java/GeneralService/OnlineServiceHandler.java
@@ -4,10 +4,22 @@ import onlineservices.OnlineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.List;
 
 @Service
 public class OnlineServiceHandler {
     @Autowired
     private List<OnlineService> activatedServices;
+
+    @PostConstruct
+    private void activateServices() {
+        activatedServices.forEach(OnlineService::onCreate);
+    }
+
+    @PreDestroy
+    private void deactivateServices() {
+        activatedServices.forEach(OnlineService::onDestroy);
+    }
 }
