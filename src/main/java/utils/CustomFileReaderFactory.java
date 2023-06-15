6e60dd1e022f09f963659d0419957379f4b4bcf6
CSandu <CristinaAlexandra.Sandu@harman.com>
2023-06-15 15:25:01 +0300

    Used ImmutableMap in file factory.

--- src/main/java/utils/CustomFileReaderFactory.java
+++ src/main/java/utils/CustomFileReaderFactory.java
@@ -1,14 +1,12 @@
 package utils;
 
+import com.google.common.collect.ImmutableMap;
 import utils.filereaders.CsvFileReader;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class CustomFileReaderFactory {
-    private static final HashMap<FileType, CustomFileReader> CUSTOM_FILE_READER = new HashMap<>(Map.of(
-            FileType.CSV, new CsvFileReader()
-    ));
+    private static final ImmutableMap<FileType, CustomFileReader> CUSTOM_FILE_READER = ImmutableMap.<FileType, CustomFileReader>builder()
+            .put(FileType.CSV, new CsvFileReader())
+            .build();
 
     public static CustomFileReader getFileReader(FileType fileType) {
         CustomFileReader fileReader = CUSTOM_FILE_READER.get(fileType);
